73088f6ceb757167e24a39f743f017d3335b89b6
muyao <55979740+muyao@users.noreply.github.com>
2026-08-03 14:09:43 +0200

    Hotfix rename to gfx.draw_screen()

--- modules/command.py
+++ modules/command.py
@@ -33,7 +33,7 @@ def process_command(command):
 	# Show help screen if help
 	elif command == "h" or command == "help":
 		# Clear screen
-		gfx.clear_screen(
+		gfx.draw_screen(
 			"Help Manual",
 			"Press any key to exit help manual"
 		)

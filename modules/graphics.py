91805a636dc5bb0e4002e1072e5585fe07b462dd
muyao <55979740+muyao@users.noreply.github.com>
2026-08-03 13:49:25 +0200

    Add more info to title

--- modules/graphics.py
+++ modules/graphics.py
@@ -1,11 +1,12 @@
 import modules.globals as g
 
-def clear_screen(bottom_prompt):
+def clear_screen(info, bottom_prompt):
 	# Clear screen
 	g.stdscr.clear()
 	
 	# Title
-	g.stdscr.addstr(0, int(g.MAX_X / 2) - 9, "The Pager of Babel")
+	title = f"The Pager of Babel - {info}"
+	g.stdscr.addstr(0, int((g.MAX_X - len(title)) / 2), title)
 
 	# Bottom prompt
 	g.stdscr.addstr(g.MAX_Y - 2, 0, bottom_prompt)

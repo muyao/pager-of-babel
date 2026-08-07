7b4043ac2b156434a2ff6da729cbcde3c80f84bc
muyao <55979740+muyao@users.noreply.github.com>
2026-08-07 16:10:20 +0200

    Limit line

--- modules/user_keypress.py
+++ modules/user_keypress.py
@@ -21,6 +21,8 @@ def handle_keypress(key: int) -> None:
 		# Page# cannot go past PAGES_PER_LOG
 		if pg.current_page > c.PAGES_PER_ENTRY:
 			pg.current_page = c.PAGES_PER_ENTRY
+		if pg.current_page >= c.PAGES_PER_ENTRY:
+			pg.line_offset = 0
 
 	elif key == ord('b'):
 		pg.current_page -= 1
@@ -28,6 +30,7 @@ def handle_keypress(key: int) -> None:
 		# Make sure page is at least 1
 		if pg.current_page < 1:
 			pg.current_page = 1
+			pg.line_offset = 0
 
 	elif key == ord('q'):
 		# Stop running
@@ -53,7 +56,7 @@ def handle_keypress(key: int) -> None:
 
 	elif key == ord('k') or key == curses.KEY_UP:
 		# Stop if we reach page 1
-		if pg.current_page == 1:
+		if pg.current_page == 1 and pg.line_offset == 0:
 			return
 
 		pg.line_offset -= 1
